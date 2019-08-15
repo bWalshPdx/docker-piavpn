daa0890d38427d6058cc4a51994956d56a0ca5a8
Brian.Walsh <databasebrian@gmail.com>
2019-08-14 18:55:11 -0700

    Basic laptop setup

---
+++ laptopStack.ps1
@@ -0,0 +1,8 @@
+﻿$env:torrentPath = ""
+$env:delugePath = ""
+$env:timezonePath = ""
+$env:piaUsername = ""
+#$env:piaPassword = ""
+$env:optionalPiaClient = ""
+
+docker stack deploy --compose-file docker-compose.yml torrentServer
